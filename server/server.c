f23d654c53ed01f221f6d9c147eceaef98aa8b7a
Justin Miller <justintime145@gmail.com>
2019-04-14 08:12:48 -0400

    Stuck some servo code in server

--- server/server.c
+++ server/server.c
@@ -8,47 +8,56 @@ int state = 0;
 
 Response * deleteCallback(Request *req)
 {
-    Response *res = malloc(sizeof(Response));
-    res->status = STATUS_NOT_IMPL;
-    res->headers = createHeaders();
-    setHeader(res->headers, "Safe", "no");
-    res->body = malloc(64);
-    strcpy(res->body, "That operation is not supported here, chump.\0");
-    res->bodyLength = strlen(res->body+1);
-    return res;
+	Response *res = malloc(sizeof(Response));
+	res->status = STATUS_NOT_IMPL;
+	res->headers = createHeaders();
+	setHeader(res->headers, "Safe", "no");
+	res->body = malloc(64);
+	strcpy(res->body, "That operation is not supported here, chump.\0");
+	res->bodyLength = strlen(res->body+1);
+	return res;
 }
 
 Response * brewCallback(Request *req)
 {
-    Response *res = malloc(sizeof(Response));
-    res->status = STATUS_OK;
-    res->headers = createHeaders();
-    setHeader(res->headers, "Safe", "yes");
-    res->body = malloc(64);
-    strcpy(res->body, "Starting some coffee for you.\0");
-    res->bodyLength = strlen(res->body+1);
-    return res;
+	Response *res = malloc(sizeof(Response));
+	res->status = STATUS_OK;
+	res->headers = createHeaders();
+	setHeader(res->headers, "Safe", "yes");
+	res->body = malloc(64);
+	strcpy(res->body, "Starting some coffee for you.\0");
+	res->bodyLength = strlen(res->body+1);
+	return res;
 }
 
 Response * postCallback(Request *req)
 {
-    return brewCallback(req);
+	printf ("Raspberry Pi wiringPi test program\n");
+	wiringPiSetupGpio();
+	pinMode (18, PWM_OUTPUT) ;
+	pwmSetMode (PWM_MODE_MS);
+	pwmSetRange (2000);
+	pwmSetClock (192);
+	pwmWrite(18,150);
+	delay(1000);
+	pwmWrite(18,200);
+	return brewCallback(req);
 }
 
 int main(int argc, char **argv)
 {
-    HTCPCPServer *server = getServer();
-    addRoute(server, METHOD_BREW, "/brew", brewCallback);
-    addRoute(server, METHOD_POST, "/brew", postCallback);
-    addRoute(server, METHOD_DELETE, "/brew", deleteCallback);
-    if(argc > 1)
-    {
-        setServerAddress(server, argv[1]);
-    }
-    startServer(server);
-    while(1)
-    {
-        sleep(1);
-        printf("Yawn\n");
-    }
+	HTCPCPServer *server = getServer();
+	addRoute(server, METHOD_BREW, "/brew", brewCallback);
+	addRoute(server, METHOD_POST, "/brew", postCallback);
+	addRoute(server, METHOD_DELETE, "/brew", deleteCallback);
+	if(argc > 1)
+	{
+		setServerAddress(server, argv[1]);
+	}
+	startServer(server);
+	while(1)
+	{
+		sleep(1);
+		printf("Yawn\n");
+	}
 }
